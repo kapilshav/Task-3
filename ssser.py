451160eb5819b0438d5033a9cbf72090c5760bb8
kapilshav <69969865+kapilshav@users.noreply.github.com>
2021-08-17 01:15:34 +0530

    Add files via upload

---
+++ ssser.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import socket,cv2,pickle,struct
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_name = socket.gethostname()
+host_ip = '192.168.29.173'
+print('HOST IP:',host_ip)
+port = 1234
+socket_address = ('192.168.29.173',port)
+
+server_socket.bind(socket_address)
+
+server_socket.listen(5)
+print("LISTENING AT:" ,socket_address)
+
+while True:
+    client_socket,addr = server_socket.accept()
+    print('GOT CONNECTION FROM:' ,addr)
+    if client_socket:
+        vid = cv2.VideoCapture(0)
+        while(vid.isOpened()):
+            img,frame = vid.read()
+            a = pickle.dumps(frame)
+            message = struct.pack("Q",len(a))+a
+            client_socket.sendall(message)
+            
+            cv2.imshow("Transmiting", frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                client_socket.close()
+
+
+# In[ ]:
+
+
+
+
